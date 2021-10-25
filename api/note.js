b161368cc62707b23b74f815bff0accd2f5520ec
Santiago Trini <santiagotrini@gmail.com>
2021-10-25 17:46:03 -0300

    agrega rutas

--- api/note.js
+++ api/note.js
@@ -5,25 +5,38 @@ const Note = require('../models/Note');
 
 router
   .route('/api/notes')
+  // buscar todas las notas
   .get((req, res) => {
     Note.find((err, notes) => {
       res.json(notes);
     })
   })
+  // crear una nota usando los datos de req.body
   .post((req, res) => {
-    res.send('Crear nota');
+    const note = new Note({
+      title: req.body.title,
+      text: req.body.text
+    });
+    note.save((err, note) => {
+      res.json(note);
+    })
   });
 
 router
   .route('/api/notes/:id')
+  // buscar una nota por id
   .get((req, res) => {
-    res.send('Una nota por ID');
+    Note.find({ id: req.params.id }, (err, note) => {
+      res.json(note);
+    });
   })
   .put((req, res) => {
     res.send('Actualizar nota');
   })
   .delete((req, res) => {
-    res.send('Borrar nota');
+    Note.findByIdAndRemove(req.params.id, (err) => {
+      res.json({ msg: 'Nota borrada' });
+    })
   });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const DB   = 'mongodb://localhost/notasapp';
 mongoose.connect(DB).then(() => console.log('DB conectada'));
 
 const app = express();
+// para que aparezca algo en req.body
+app.use(express.json());
 
 app.use('/', require('./api/note'));
 
